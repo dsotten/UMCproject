b0bc7f77cc77a224fab390e761c694dbd79c4bd2
Jaidyn Vankirk <vankirk.jaidyn@gmail.com>
2024-10-21 11:59:38 -0400

    Updated the file to find bars closest to the alumni house and to route from the alumni house to the Govenor's on North Henry St Apartment Complex

--- mapsapitest.py
+++ mapsapitest.py
@@ -1,22 +1,39 @@
 import requests
 
 API_KEY = 'AIzaSyBzoCUm8NNP68qFTVdWHVlX-MfNIjXUwOE'
-location = '40.748817,-73.985428'  # New York City (example)
-radius = 1500  # 1.5 km
+location = '37.2744,-76.714'  # New York City (example)
+radius = 300  # 1.5 km
 place_type = 'bar'
 
 radius_url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={location}&radius={radius}&type={place_type}&key={API_KEY}'
 
-routing_url = f'https://maps.googleapis.com/maps/api/directions/json?origin=40.748817,-73.985428&destination=40.785091,-73.968285&key={API_KEY}'
+routing_url = f'https://maps.googleapis.com/maps/api/directions/json?origin=37.2744,-76.7143&destination=37.27732,-76.70697&key={API_KEY}'
 
-route_request = requests.get(routing_url)
-route_json = route_request.json()
+radius_request = requests.get(radius_url)
+bars = []
 
-# print(data)
+if radius_request.status_code == 200:
+    places = radius_request.json()
+    if places['status'] == 'OK':
+        bars = places['results']
+    else:
+        print("Error in response: ", places['status'])
+else:
+    print("Request failed: ", radius_request.status_code)
+
+
+if bars:
+    print("Bars found:")
+    for bar in bars:
+        print(bar['name'], "-", bar['vicinity'])
+
 
 # Print out the results
 # for place in data['results']:
 #     print(place['name'], '-', place['vicinity'])
+
+route_request = requests.get(routing_url)
+route_json = route_request.json()
 
 if route_json['status'] == 'OK':
     # Get the first route
