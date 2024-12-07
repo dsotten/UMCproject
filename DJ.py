9b6bef68be947ad9c5cc4b448c1d9e952b8b8a30
Jaidyn Vankirk <vankirk.jaidyn@gmail.com>
2024-12-07 12:54:17 -0500

    Add files via upload

--- DJ.py
+++ DJ.py
@@ -3,78 +3,61 @@ from typing import Final
 from queue import PriorityQueue
 import math
 import json
-import heapq
+
+#This file holds our last implementation and most expensive implementation. We use dynamic grid size and routing.
 
 #Harversine's Formula - This formula is used to convert coordinates into meters. - Not as precise below 12 meters, but still relatively accurate.
-def coord_to_m(lat1, long1, lat2, long2):
+def coord_to_m(long1, lat1, long2, lat2):
     RADIUS_OF_EARTH: Final = 6371000 #in M
-    lat1 = math.radians(lat1)
-    lat2 = math.radians(lat2)
     long1 = math.radians(long1)
     long2 = math.radians(long2)
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
     delt_lat = lat2 - lat1
     delt_long = long2 - long1
     a = (math.sin(delt_lat/2))**2 + math.cos(lat1) * math.cos(lat2) * (math.sin(delt_long/2))**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     return c * RADIUS_OF_EARTH #1000 to conver to meters
 
-#Looks through the bar list to see if there is a bar close to the point being looked at and adds a weight of .5. (The .5 is subject to change.)
-def calc_bar_weight(bar_coords, pot_x, pot_y, grid_size):
-    BAR_WEIGHT: Final = 15 #Has to be high for shorter distances, but might not need to be shorter for smaller distances.
-    weight = 0
-    for bar in bar_coords:
-        distance_x = abs(bar[0]-pot_x)
-        distance_y = abs(bar[1]-pot_y)
-        if distance_x < grid_size*5: #Need to change the weight calculation.
-            weight += distance_x * BAR_WEIGHT
-        elif distance_y < grid_size:
-            weight += BAR_WEIGHT
-    # print('Weight Added:' + str(weight))
-    return weight
+#Looks through the list to see if there is an avoided establishment close to the point being looked at and adds a weighted constant. - Uses this to only have to make one API call.
+def calc_weight(coords, long, lat, grid_size):
+    WEIGHT_CONSTANT: Final = 10 #Constant currently but plan to update for future updates.
+    weight_addition = 0 #initially
+    for place in coords:
+        distance_x = abs(place[0]-long) #Should return a negative or positive value.
+        distance_y = abs(place[1]-lat)
+        if distance_x < grid_size*2 or  distance_y < grid_size:
+            weight_addition += coord_to_m(lat, long, place[0], place[1]) * WEIGHT_CONSTANT
+    return weight_addition
 
-#For now we are only going to use bars. - Just makes things easier
-#Might have issues with bigger distances.
-def get_route(origin_x, origin_y, dest_x, dest_y, avoid_place,opennow=True):
-    API_KEY: Final = 'AIzaSyCWNkEIFpv-qXrdqTZlHGJrKFP9tm9eulU'
+#The main program that gets the alcohol avoidance route.
+#Input(s):
+#avoid_place - The list of the type of establishments your route is to avoid.
+def get_route(origin_long, origin_lat, dest_long, dest_lat, avoid_place, opennow=True):
+    API_KEY: Final = 'AIzaSyDUWGT6bmVDHC3vST7oSW9eK2vhzvWlI8M' 
 
-    origin_x = origin_x
-    origin_y = origin_y
-    destination_x = dest_x
-    destination_y = dest_y
+    #Defines the distance away it will look from a point.
+    grid_size = .0001 #Dynamic grid size por favor
 
-    #Defines what is the bigger x and y.
-    if(destination_x > origin_x):
-        bigger_x = destination_x
-    else:
-        bigger_x = origin_x
 
-    if(destination_y > origin_y):
-        bigger_y = destination_y
-    else:
-        bigger_y= origin_y
-
+    radius_int = coord_to_m(origin_long,origin_lat,dest_long, dest_lat) #Have to have the meters away to give to the api
+    #A 2D array that in the first dimension will hold the type of places to avoid and in the second dimension holds the lists of coordinates where they establishment is in the radius.
     avoid_place_lst = []
-    #Formulates the url that looks for bars in the areas. Added open now.
+    
+    #Gets coordinates of establishments for each place type.
     for place_type in avoid_place:
-        coord = str(origin_x)+','+str(origin_y)
-        radius_int = coord_to_m(origin_x,origin_y,destination_x,destination_y)
+        #Everything needs to be in String format to implement in the URL
+        coord = str(origin_long)+','+str(origin_lat)
         radius = str(radius_int)
-        #Set to 25 miles or 40233.6 meters
-        # print("Radius:", radius)
-        if radius_int < 40233.6:
-            grid_size = .00055 #May need to be changed
-        else:
-            if(bigger_x>bigger_y):
-                grid_size = bigger_x/20
-            else:
-                grid_size = bigger_y/20
         
+        #Only includes establishments that are open based on the time, else includes all establishments in the area.
         if opennow:
             radius_url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={coord}&radius={radius}&type={place_type}&opennow&key={API_KEY}'
         else:
             radius_url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={coord}&radius={radius}&type={place_type}&key={API_KEY}'
 
-        #Makes the radius request. Gets bars in the area surrounding the origin and puts them into the bar_coords list. The radius is how far away the url looks away from the destination is.
+        #Makes the radius request. Gets bars in the area surrounding the origin and puts them into the bar_coords list.
+        #If an error occurs when making the bar request, it is printed in the console.
         coords = []
         radius_request = requests.get(radius_url)
         if radius_request.status_code == 200:
@@ -83,11 +66,11 @@ def get_route(origin_x, origin_y, dest_x, dest_y, avoid_place,opennow=True):
                 bars = places['results']
                 if bars:
                     for bar in bars:
-                        lat = float(bar['geometry']['location']['lat'])
                         lng = float(bar['geometry']['location']['lng'])
-                        coords.append([lat, lng])
+                        lat = float(bar['geometry']['location']['lat'])
+                        coords.append((lng, lat))
             elif places['status'] == 'ZERO_RESULTS':
-                print('No '+str(place_type)+'s') #Probably need to come up with situatiosn for these
+                print('No '+str(place_type)+'s') # I think your implementation has some extra stuff.
             else:
                 print("Error in response: ", places['status'])
         else:
@@ -95,158 +78,133 @@ def get_route(origin_x, origin_y, dest_x, dest_y, avoid_place,opennow=True):
         if len(coords) > 0:
             avoid_place_lst.append(coords)
 
-    #Current node
-    current_node_str = str(origin_x) + ',' + str(origin_y)
-    current_node = [origin_x, origin_y]
+    #Current node - Starts at the origin
+    current_node_str = str(origin_long) + ',' + str(origin_lat)
+    current_node = (origin_long, origin_lat)
 
-    #Keeps track of connections - to not rereview places
-    connection_graph = {current_node_str: ''} #?
-    #Keep tracks of weight - to add on the amount it currently takes to get there
+    #Keeps track of connections - main purpose is to not rereview places and to back track once the algorithm is over. Holds the current_lat_and_long and the coordinates prior to get there.
+    connection_graph = {current_node_str: ''} 
+    #Keep tracks of weight - main purpose is to add on the weight of previous nodes. - Weight is in meters
     weight_graph = {current_node_str: 0}
-    #Puts weights into here
+    #Puts (weights, node_name) into here to make decisions on what node to traverse to next.
     pq = PriorityQueue()
 
     check_coords = ''
-
-    x_variant = grid_size
-    y_variant = grid_size
-
-    #I think there is an issue here
-    i = 0
+    #Will use these when creating the route
     waypoints = ''
-    if len(avoid_place_lst) > 0:
-        print("Num places to avoid: "+str(len(avoid_place_lst)))
-        while abs((abs(current_node[0])+ abs(current_node[1])) - (abs(destination_x)+ abs(destination_y))) > grid_size and i <= 50:
-            #Test line
-            print(abs(abs((current_node[0]) + abs(current_node[1])) - (abs(destination_x)+ abs(destination_y))))
-            #Gets the locations and the weights of nodes it wants to look at surrounding the current node.
-            check_coords += str(current_node[0]+x_variant) + ',' + str(current_node[1]+y_variant)
-            check_coords += '|'+str(current_node[0]-x_variant)+','+str(current_node[1]+y_variant)
-            check_coords += '|'+str(current_node[0]+x_variant)+','+str(current_node[1]-y_variant)
-            check_coords += '|'+str(current_node[0]-x_variant)+','+str(current_node[1]-y_variant)
-            check_coords += '|'+ str(current_node[0]+x_variant) + ',' + str(current_node[1])
-            check_coords += '|'+ str(current_node[0]-x_variant) + ',' + str(current_node[1])
-            check_coords += '|'+ str(current_node[0]) + ',' + str(current_node[1]+y_variant)
-            check_coords += '|'+ str(current_node[0]) + ',' + str(current_node[1]-y_variant)
 
-            #Gets the nearest roads from the points defined above.
+    api_check = 0
+
+    if len(avoid_place_lst) > 0:
+        #Makes sure the longitude and latitude are not within a gride size and checks to make sure the api is not called more than 50 times.
+        while abs(current_node[0] -  dest_long) > grid_size and abs(current_node[1] - dest_lat) > grid_size and api_check <= 50:
+            #Gets the locations and the weights of nodes it wants to look at surrounding the current node. Format:
+            # ***
+            # *X*
+            # ***
+            # * - The places checked.
+            # X - The current cordinate.
+            check_coords += str(current_node[0]+grid_size) + ',' + str(current_node[1]+grid_size)
+            check_coords += '|'+str(current_node[0]-grid_size)+','+str(current_node[1]+grid_size)
+            check_coords += '|'+str(current_node[0]+grid_size)+','+str(current_node[1]-grid_size)
+            check_coords += '|'+str(current_node[0]-grid_size)+','+str(current_node[1]-grid_size)
+            check_coords += '|'+ str(current_node[0]+grid_size) + ',' + str(current_node[1])
+            check_coords += '|'+ str(current_node[0]-grid_size) + ',' + str(current_node[1])
+            check_coords += '|'+ str(current_node[0]) + ',' + str(current_node[1]+grid_size)
+            check_coords += '|'+ str(current_node[0]) + ',' + str(current_node[1]-grid_size)
+
+            #Put into the proper URL and trys to get the roads. If an error occurs, it is printed in the console.
             closest_road_url = f'https://roads.googleapis.com/v1/nearestRoads?points={check_coords}&key={API_KEY}'
-            i += 1
             try:
                 road_request = requests.get(closest_road_url)
                 road_request.raise_for_status()  # Raise an error for HTTP codes 4xx/5xx
                 road_request_data = road_request.json()
-                with open("sample.json", "w") as outfile:
-                    json.dump(road_request_data, outfile)
             except requests.exceptions.RequestException as e:
                 print(f"Error: {e}")
-
-            #Potential issue - For 2 way roads, they are listed twice. Need to formulate some way to not double them, but also not skipping over roads that have the same nearest road.
-            #Goes through the data
+            
+            #Adds the new data to the connection_graph, weight_graph, and pq.
             if "snappedPoints" in road_request_data:
-
                 for snapped_point in road_request_data["snappedPoints"]:
-                    #Gets string version of the closest road
+                    longitude = snapped_point["location"]["longitude"] 
                     latitude = snapped_point["location"]["latitude"]
-                    longitude = snapped_point["location"]["longitude"]
-                    lat_long = str(latitude)+ ','+ str(longitude)
-                    #Test Line
-                    # print("Coords:" + lat_long)
-                    
+                    long_lat = str(latitude)+ ','+ str(longitude) #I don't know why the json is flipping them
+
                     #Checks to see if the road is already in the graph
-                    if lat_long not in connection_graph:
-                        #Adds it as a new node to the connection graph and connects it with the node that found it. Should not add to the pq again.
-                        connection_graph.update({lat_long: current_node_str})
-                        weight = abs(destination_x - latitude) + abs(destination_y - longitude) #Calculates the distance from the road
+                    if long_lat not in connection_graph:
+                        #Adds it as a new node to the connection graph and connects it with the node that found it.
+                        connection_graph.update({long_lat: current_node_str})
+                        #Gets the weight to the road destination
+                        
+                        routing_url = f'https://maps.googleapis.com/maps/api/directions/json?origin={long_lat}&destination={str(dest_long)},{str(dest_lat)}&units=metric&key={API_KEY}'
+
+                        route_request = requests.get(routing_url)
+                        route_json = route_request.json()
+
+                        if route_json['status'] == 'OK':
+                            # Get the first route
+                            route = route_json['routes'][0]
+                            leg = route['legs'][0]
+                            dest_weight = float(leg['distance']['value'])
+                        else:
+                            dest_weight = 100
+
+                        #Necessary to look for the wide variety of places.
                         place_weight = 0
                         for place in avoid_place_lst:
-                            place_weight += calc_bar_weight(place, latitude, longitude, grid_size)
-                        weight_graph.update({lat_long: (weight + place_weight + (weight_graph.get(current_node_str)))})
-                        #Test Line
-                        # print("Weight:" + str(weight))
-                        #Puts it in the priority queue with its associated weight
-                        pq.put((weight_graph.get(lat_long), lat_long))
+                            place_weight += calc_weight(place, latitude, longitude, grid_size)
+                        weight_graph.update({long_lat: (place_weight + (weight_graph.get(current_node_str)))})
+                        pq.put((weight_graph.get(long_lat), long_lat))
                 
-            #Gets the next current_node.
+            #Prints if this error occurs
             if pq.empty():
                 print("Priority queue is empty; terminating.")
                 break
             new_current_node = pq.get()
-            # print(new_current_node)
             current_node_str = new_current_node[1]
             ll = current_node_str.split(',')
-            current_node = [float(ll[0]),float(ll[1])]
-            # print("ans:" + str(abs(abs((current_node[0]) + abs(current_node[1])) - (abs(destination_x)+ abs(destination_y)))))
-            # print("Current_Node:", current_node_str)
-            #Divides the string of the next node into 2.
+            current_node = (float(ll[0]),float(ll[1]))
             check_coords = ''
-            # i += 1
+            api_check += 1
 
-        past_current_node = min(weight_graph)
+        past_current_node = min(weight_graph) #Gets the min in case the API calls terminated the search, leaving it incomplete.
         waypoints = past_current_node
-        j = 0
-        while past_current_node != '' and j < 50:
+        #Adds the way points to a string so the URL can use it.
+        while past_current_node != '':
             waypoints += '|' + past_current_node
             past_current_node = connection_graph.get(past_current_node,'')
             past_current_node = connection_graph.get(past_current_node,'')
-            j+= 1
 
-    print('While loop finished')
-
-    #Open now
-    #Gets the route based off of the way points.
+    #Gets the route based off of the way points (if available)
     if waypoints != '':
-        routing_url = f'https://maps.googleapis.com/maps/api/directions/json?origin={str(origin_x)},{str(origin_y)}&destination={str(destination_x)},{str(destination_y)}&waypoints={waypoints}&units=metric&key={API_KEY}'
+        routing_url = f'https://maps.googleapis.com/maps/api/directions/json?origin={str(origin_long)},{str(origin_lat)}&destination={str(dest_long)},{str(dest_lat)}&waypoints={waypoints}&units=metric&key={API_KEY}'
     else:
-        routing_url = f'https://maps.googleapis.com/maps/api/directions/json?origin={str(origin_x)},{str(origin_y)}&destination={str(destination_x)},{str(destination_y)}&units=metric&key={API_KEY}'
-    i += 1
+        routing_url = f'https://maps.googleapis.com/maps/api/directions/json?origin={str(origin_long)},{str(origin_lat)}&destination={str(dest_long)},{str(dest_lat)}&units=metric&key={API_KEY}'
 
     route_request = requests.get(routing_url)
     route_json = route_request.json()
 
-    #Look at why it is not printing the final destination
+    #Returns route and API calls made for searching
     if route_json['status'] == 'OK':
         # Get the first route
         route = route_json['routes'][0]
 
         return {
             'route_json': route,
-            'num_api_calls': i
+            'num_api_calls': api_check*8
         }
-        # print("\nDirections:")
-        # # Print out the total distance and duration
-        # for leg in route['legs']:
-        #     print(f"Total Distance: {leg['distance']['text']}")
-        #     print(f"Total Duration: {leg['duration']['text']}")
-        
-        #     # Print each step of the route
-        #     for step in leg['steps']:
-        #         instruction = step['html_instructions']  # Contains HTML instructions
-        #         distance = step['distance']['text']  # Step distance
-        #         duration = step['duration']['text']  # Step duration
-
-        #         # Strip HTML tags from the instruction (if needed)
-        #         import re
-        #         clean_instruction = re.sub('<.*?>', '', instruction)  # Remove HTML tags
-
-        #         # Print step details
-        #         print(f"- {clean_instruction} ({distance}, {duration})")
-        #     print("New leg")
     else:
         print(f"Error: {route_json['status']}")
         return {
             'route_json': None,
-            'num_api_calls': i
+            'num_api_calls': api_check
         }
-        
+
+#Some example test code
 if __name__ == '__main__':
-    #DownTown Art Museum - 37.2686°N 76.7048°W
-    #Zable Stadium - 37.2730556 ″N -76.7133333″W
-    #Current Example - Zable Stadium to Jay's Apartment
-    origin_x = 37.2686
-    origin_y = -76.7133333 #Zable Stadium
-    destination_x = 37.27732
-    destination_y = -76.70697 #Jay's Apartment
+    origin_long = 37.2686
+    origin_lat = -76.7133333 #Not Zable Stadium
+    dest_long = 37.27732
+    dest_lat = -76.70697 #Apartment Complex
     avoid_place = ['bar']
 
-    get_route(origin_x, origin_y, destination_x, destination_y, avoid_place)
+    get_route(origin_long, origin_lat, dest_long, dest_lat, avoid_place)
